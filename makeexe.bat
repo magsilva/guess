4324d0f1b6346901ebf1ec96c56b7f3b3c8eef3b
Eytan Adar <eytanadar@gmail.com>
2005-10-18 00:25:04 +0000

    *** empty log message ***

---
+++ makeexe.bat
@@ -0,0 +1,2 @@
+"c:\program files\7-zip\7z.exe" a guess.7z guess-install.jar launcher.ini launcher-Win32.exe
+copy /B 7zS.sfx + config.txt + guess.7z guess-install.exe
--- src/com/hp/hpl/guess/Version.java
+++ src/com/hp/hpl/guess/Version.java
@@ -3,5 +3,5 @@ package com.hp.hpl.guess;
 public abstract class Version {
 	
 	public static String MAJOR_VERSION = ".5";
-	public static String MINOR_VERSION = "20051017-0244";
+	public static String MINOR_VERSION = "20051017-0516";
 }
