aa78feb7712afd2a498411cbf1dfe135da05090a
Eytan Adar <eytanadar@gmail.com>
2005-10-28 23:13:40 +0000

    *** empty log message ***

--- guess.bat
+++ guess.bat
@@ -1,4 +1,6 @@
 @echo off
 @rem set GUESS_HOME=c:\program files\GUESS
 set GUESS_LIB=%GUESS_HOME%\lib
+cd %GUESS_LIB%
 java "-Dpython.home=%GUESS_HOME%\src" -DgHome="%GUESS_HOME%" -jar guess.jar %1 %2 %3 %4 %5 %6 %7
+cd ..
