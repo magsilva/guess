311a8fdf92570ca946d5100858f56f6febad3f43
Eytan Adar <eytanadar@gmail.com>
2007-08-09 18:39:28 +0000

    option to run guess without opengl (some people have crashes)

--- guess-console-noopengl.bat
+++ guess-console-noopengl.bat
@@ -5,5 +5,5 @@
 @rem set GUESS_HOME=c:\program files\GUESS
 set GUESS_LIB=%GUESS_HOME%\lib
 set GCLASSPATH=guess.jar;%GUESS_LIB%\guess.jar;%GUESS_LIB%\piccolo.jar;%GUESS_LIB%\piccolox.jar;%GUESS_LIB%\jung.jar;%GUESS_LIB%\commons-collections.jar;%GUESS_LIB%\hsqldb.jar;%GUESS_LIB%\freehep-all.jar;%GUESS_LIB%\colt.jar;%GUESS_LIB%\prefuse.jar;%GUESS_LIB%\TGGraphLayout.jar;%GUESS_LIB%\looks.jar;%GUESS_LIB%\mascoptLib.jar;%GUESS_LIB%\jfreechart.jar;%GUESS_LIB%\jide-components.jar;%GUESS_LIB%\jide-common.jar;%GUESS_LIB%\forms.jar;%GUESS_LIB%\jcommon.jar
-java -classpath "%GCLASSPATH%" "-Dpython.home=%GUESS_HOME%\src" -Dsun.java2d.opengl=True -DgHome="%GUESS_HOME%" com.hp.hpl.guess.Guess --console %1 %2 %3 %4 %5 %6 %7
+java -classpath "%GCLASSPATH%" "-Dpython.home=%GUESS_HOME%\src" -DgHome="%GUESS_HOME%" com.hp.hpl.guess.Guess --console %1 %2 %3 %4 %5 %6 %7
 
