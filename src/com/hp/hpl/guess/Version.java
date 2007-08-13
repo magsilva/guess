fa17a747ffb47fecae460491fe76b9e5ac67c956
Eytan Adar <eytanadar@gmail.com>
2007-08-13 22:46:11 +0000

    update for current release, change to build file

--- src/com/hp/hpl/guess/Version.java
+++ src/com/hp/hpl/guess/Version.java
@@ -3,5 +3,5 @@ package com.hp.hpl.guess;
 public abstract class Version {
 	
 	public static String MAJOR_VERSION = "1.0.3beta";
-	public static String MINOR_VERSION = "20070810-0417";
+	public static String MINOR_VERSION = "20070813-1150";
 }
